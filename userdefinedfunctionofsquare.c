7f0ab15fa026941d810293d4f487cf3f45a5491d
kumartatikonda1119 <147791707+kumartatikonda1119@users.noreply.github.com>
2023-12-15 11:22:02 +0530

    Add files via upload

---
+++ userdefinedfunctionofsquare.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int area(int);
+ int main()
+ {
+ 	int s;
+ 	scanf("%d",s);
+ int 	res=area(s);
+ 	printf("%d",res);
+ 	
+ 	
+ }
+ int area(int a)
+ {
+ 	int f;
+ 	f=a*a;
+ 	return f;
+ 	
+ }
